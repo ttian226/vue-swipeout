6432453eea3d68f08fbe7fc7c14b1562c7cc9712
wangxu <ttian226@gmail.com>
2018-01-10 15:51:46 +0800

    remove postcssrc

--- demo/demo.vue
+++ demo/demo.vue
@@ -1,47 +1,47 @@
 <template>
   <div class="demo">
     <v-swipeout
-  :autoClose="false"
-  :style="{ backgroundColor: 'white' }"
-  :left="[
-    {
-      text: 'read',
-      onPress: readBtn,
-      style: { backgroundColor: 'blue', color: 'white' }
-    },
-    {
-      text: 'reply',
-      onPress: replayBtn,
-      style: { backgroundColor: 'green', color: 'white' }
-    }
-  ]"
-  :right="[
-    {
-      text: 'more',
-      onPress: moreBtn,
-      style: { backgroundColor: 'orange', color: 'white' }
-    },
-    {
-      text: 'delete',
-      onPress: deleteBtn,
-      style: { backgroundColor: 'red', color: 'white' }
-    }
-  ]"
-  :onOpen="openCb"
-  :onClose="closeCb"
->
-  <div 
-    :style="{
-      height: 44,
-      backgroundColor: 'white',
-      lineHeight: '44px',
-      borderTop: '1px solid #dedede',
-      borderBottom: '1px solid #dedede',
-    }"
-  >
-    swipe out simple demo
-  </div>
-</v-swipeout>
+      :autoClose="false"
+      :style="{ backgroundColor: 'white' }"
+      :left="[
+        {
+          text: 'read',
+          onPress: readBtn,
+          style: { backgroundColor: 'blue', color: 'white' }
+        },
+        {
+          text: 'reply',
+          onPress: replayBtn,
+          style: { backgroundColor: 'green', color: 'white' }
+        }
+      ]"
+      :right="[
+        {
+          text: 'more',
+          onPress: moreBtn,
+          style: { backgroundColor: 'orange', color: 'white' }
+        },
+        {
+          text: 'delete',
+          onPress: deleteBtn,
+          style: { backgroundColor: 'red', color: 'white' }
+        }
+      ]"
+      :onOpen="openCb"
+      :onClose="closeCb"
+    >
+      <div 
+        :style="{
+          height: 44,
+          backgroundColor: 'white',
+          lineHeight: '44px',
+          borderTop: '1px solid #dedede',
+          borderBottom: '1px solid #dedede',
+        }"
+      >
+        swipe out simple demo
+      </div>
+    </v-swipeout>
   </div>
 </template>
 
